0f6c1aeaeafbc0f88a7c0b5e2a416748b81dcd89
zbahati <bahatizirimwabagabo@gmail.com>
2024-06-25 12:59:54 +0200

    Add configuration of typeorm to work with relational db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+    TypeOrmModule.forRootAsync({
+    useFactory : (configService: ConfigService) =>({
+      type: 'postgres',
+      host: configService.getOrThrow('HOST'),
+      port: configService.getOrThrow('PORT'),
+      username: configService.getOrThrow('USER'),
+      password: configService.getOrThrow('PASSWORD'),
+      database: configService.getOrThrow('DB'),
+      autoLoadEntities: true,
+      synchronize: true  
+    }), 
+    inject: [ConfigService]
+  })],
   controllers: [],
   providers: [],
 })
